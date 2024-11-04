0a3996e89f3eeb7194e05a16cff2872e7bf71869
Koloreczek <92329050+Koloreczek@users.noreply.github.com>
2024-11-04 14:00:35 +0100

    Add files via upload

---
+++ EE.09-02-24.01-SG/lotnisko.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Port Lotniczy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+
+    <div class="baner1">
+        <img src="zad5.png" alt="logo lotnisko">
+    </div>
+
+    <div class="baner3">
+        <h3>Przydatne linki</h3>
+        <a target="_blank" href="kwarendy.txt">Pobierz</a>
+</div>
+    <div class="baner2">
+        <h1>Przyloty</h1>
+    </div>
+
+   
+
+    <div class="glowny">
+         <table>
+<tr>
+<th>
+   CZAS
+</th>
+<th>
+    KIERUNEK
+</th>
+<th>
+    NUMER REJSU
+</th>
+<th>
+   STATUS
+</th>
+
+</tr>
+
+         </table>
+    </div>
+
+    <div class="stopka1">
+
+    </div>
+    <div class="stopka2">
+        Autor: 21312314512512
+    </div>
+    
+</body>
+</html>
