7148a8a81f60e16a5601f17ac2c021bccab48335
Koloreczek <92329050+Koloreczek@users.noreply.github.com>
2024-11-20 15:12:23 +0100

    Add files via upload

---
+++ 2 z wiosny/skrypt.js
@@ -0,0 +1,37 @@
+function wyslij() {
+    const wiadomoss = document.getElementById('wiadomosc');
+    const chatt = document.getElementById('chat');
+    
+    const pojaw = document.getElementById('Nastepny');
+    document.getElementById(Nastepny).style.visibility = "hidden";
+    const para = document.createElement("p");
+para.innerText = "This is a paragraph";
+document.body.appendChild(para);
+
+    messageInput.value = '';
+}
+
+function generuj(){
+
+}
+
+
+const odpowiedzi = [
+    "Świetnie!",
+
+    "Kto gra główną rolę?",
+
+    "Lubisz filmy Tego reżysera?",
+
+    "Będę 10 minut wcześniej",
+
+    "Może kupimy sobie popcorn?",
+
+    "Ja wolę Colę",
+
+    "Zaproszę jeszcze Grześka",
+
+    "Tydzień temu też byłem w kinie na Diunie",
+
+    "Ja funduję bilety"
+];
